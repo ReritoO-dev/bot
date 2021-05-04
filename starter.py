2f8169e9634ffffc14fa9ff7965ba4a948873177
ReritoO-dev <77019976+ReritoO-dev@users.noreply.github.com>
2021-05-04 15:36:52 +0300

    Update starter.py

--- starter.py
+++ starter.py
@@ -12,4 +12,26 @@ for cog in cogs:
     except:
         print(f"{cog}: inmedi morq {str(e)}")
 
-client.run()
+def owner(ctx):
+    return int(ctx.author.id) in Config.OWNERIDS
+
+@bot.command(aliases = ["retard"])
+@commands.check(owner)
+async def restart(ctx):
+    yenile = discord.Embed(
+        title = "Yenileniyor...",
+        color = Config.MAINCOLOR
+    )
+    msg = await ctx.send(embed = yenile)
+    for cog in cogs:
+        bot.reload_extension("cogs." + cog)
+        restarting.add_field(name = f"{cog}", value = "✅ Coglari kontrol ettim!!")
+        await msg.edit(embed = yenile)
+    importlib.reload(Config)
+    restarting.add_field(name="Config", value="Reloaded")
+    restarting.title = "Bot Yenilendi"
+    await msg.delete(delay = 2)
+    if ctx.guild != None:
+        await ctx.message.delete(delay = 2)
+
+client.run(TOKEN)
