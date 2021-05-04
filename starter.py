93f749ff02c57dd3c4ab9005055fbbe69bc56069
ReritoO-dev <77019976+ReritoO-dev@users.noreply.github.com>
2021-05-04 15:26:46 +0300

    Update starter.py

--- starter.py
+++ starter.py
@@ -1,7 +1,15 @@
 import discord
 import asyncio
 from discord.ext import commands
+from ayarlar import *
 
 client = commands.Bot(command_prefix="!")
 
-client.run("")
+cogs = ["cogs.eglence"]
+for cog in cogs:
+    try:
+        client.load_extension(cog)
+    except:
+        print(f"{cog}: inmedi morq {str(e)}")
+
+client.run()
